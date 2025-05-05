734c409a29086c6e1a87699ba4a28f931b5b8fed
magnai_r <r.magnaibayar@gmail.com>
2025-05-05 11:29:20 +0800

    useRef deerh aldaag zassan

--- frontend/src/app/(auth)/login/_components/Login.tsx
+++ frontend/src/app/(auth)/login/_components/Login.tsx
@@ -3,17 +3,16 @@ import axios from "axios";
 import { ChevronLeft } from "lucide-react";
 import { useRef } from "react";
 export const Login = ({}) => {
-  const emailRef = useRef();
-  const passwordRef = useRef();
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
   const handleOnClick = async () => {
     const response = await axios.post("http://localhost:8000/login", {
-      email: emailRef.current.value,
-      password: passwordRef.current.value,
+      email: emailRef.current!.value,
+      password: passwordRef.current!.value,
     });
     localStorage.setItem("token", response.data.token);
   };
-  localStorage.clear();
   return (
     <div className=" flex w-104 flex-col justify-center items-start gap-6">
       <button className="flex items-start border-solid rounded-md size-9 border-[#E4E4E7]">
